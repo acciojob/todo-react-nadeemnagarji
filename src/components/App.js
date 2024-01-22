aeda1a8daf45d67ca527607acaf820128dbcc803
Nadeem <nadeemnagarji@gmail.com>
2024-01-22 13:49:40 +0530

    Assignment solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [input,setInput] = useState("")
+  let [todos,setTodos] = useState([])
+
+  let handleTodo = ()=>{
+      if(input !==""){
+        let todo = input
+        setInput("")
+        setTodos(prev=>[todo,...prev])
+      }
+  }
+
+  let handleDelete = (index)=>{
+      let updatedTodos = todos.filter((todo,i)=> i!==index )
+      setTodos(updatedTodos)
+      console.log(updatedTodos);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="main-container">
+       {/* Do not remove the main div */} 
+      <h3>To-Do-List</h3>
+      <div>
+      <input type="text" value={input}  onChange={(e)=>setInput(e.target.value)}/>
+      <button onClick={handleTodo}>Add Todo</button> </div>
+      {todos&& todos.length>0 && todos.map((todo,index)=>{
+        return <div className="Todos" key={index}><p>{todo}</p> <button onClick={()=>handleDelete(index)}>delete</button></div>
+      })}
     </div>
   )
 }
